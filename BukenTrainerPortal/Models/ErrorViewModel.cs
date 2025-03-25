412575fff0339db167e0bfc2593cab5e5835bf1a
grez-lucas <lucasgrez3010@gmail.com>
2025-03-25 18:47:41 -0300

    initial commit

---
+++ BukenTrainerPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BukenTrainerPortal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
