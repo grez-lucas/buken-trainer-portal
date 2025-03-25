f4984c0a2df000353d40caeaa7157d0fea0086a0
grez-lucas <lucasgrez3010@gmail.com>
2025-03-25 19:19:22 -0300

    feat(models): Add Athlete, CheckIn and Coach models

---
+++ BukenTrainerPortal/Models/CheckIn.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace BukenTrainerPortal.Models
+{
+    public class CheckIn
+    {
+        public int ID { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public required Athlete Athlete { get; set; }
+        public required Coach Coach { get; set; }
+    }
+}
---
+++ BukenTrainerPortal/Models/Coach.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace BukenTrainerPortal.Models
+{
+    public class Coach
+    {
+        public int ID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public ICollection<CheckIn>? CheckIns { get; set; }
+    }
+}
