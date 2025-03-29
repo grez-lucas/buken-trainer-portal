3652b3594afc5c3dc53c6ee3a967fe6f889d08c0
grez-lucas <lucasgrez3010@gmail.com>
2025-03-28 21:04:17 -0300

    feat(AthleteController): Inject db to class builder

--- BukenTrainerPortal/Controllers/AthleteController.cs
+++ BukenTrainerPortal/Controllers/AthleteController.cs
@@ -1,3 +1,5 @@
+using BukenTrainerPortal.Data;
+using BukenTrainerPortal.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BukenTrainerPortal.Controllers;
@@ -5,14 +7,17 @@ namespace BukenTrainerPortal.Controllers;
 public class AthletesController : Controller
 {
     private readonly ILogger<AthletesController> _logger;
+    private readonly ApplicationDbContext _db;
 
-    public AthletesController(ILogger<AthletesController> logger)
+    public AthletesController(ILogger<AthletesController> logger, ApplicationDbContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public IActionResult Index()
     {
+        List<Athlete> objAthleteList = _db.Athletes.ToList();
         return View();
     }
 }
