d164e23a5bef582f4de816996f03ad96bdc3ac5b
grez-lucas <lucasgrez3010@gmail.com>
2025-04-01 18:38:12 -0300

    feat(models): Add check-in form fields

---
+++ BukenTrainerPortal/Migrations/20250401213745_add-check-in-fields.cs
@@ -0,0 +1,306 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BukenTrainerPortal.Migrations
+{
+    /// <inheritdoc />
+    public partial class addcheckinfields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheckIns_Athletes_AthleteID",
+                table: "CheckIns");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheckIns_Coaches_CoachID",
+                table: "CheckIns");
+
+            migrationBuilder.RenameColumn(
+                name: "CoachID",
+                table: "CheckIns",
+                newName: "CoachId");
+
+            migrationBuilder.RenameColumn(
+                name: "AthleteID",
+                table: "CheckIns",
+                newName: "AthleteId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CheckIns_CoachID",
+                table: "CheckIns",
+                newName: "IX_CheckIns_CoachId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CheckIns_AthleteID",
+                table: "CheckIns",
+                newName: "IX_CheckIns_AthleteId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ActivityAdherence",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AffectedMuscleOrSession",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "AverageWeight",
+                table: "CheckIns",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "DiastolicBloodPressure",
+                table: "CheckIns",
+                type: "smallint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DietAdherence",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "EnergyLevel",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "FastingGlucoseLevels",
+                table: "CheckIns",
+                type: "numeric",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "HungerLevel",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MenstrualCyclePhase",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "MuscleRecovery",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NextWeekChallenges",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OffPlanMeals",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "OtherInformation",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlansToAddressChalllenges",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProgressPictureURLs",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SleepQuality",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "StressLevel",
+                table: "CheckIns",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<short>(
+                name: "SystolicBloodPressure",
+                table: "CheckIns",
+                type: "smallint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WeeklyAchievements",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "WeeklyChallenges",
+                table: "CheckIns",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheckIns_Athletes_AthleteId",
+                table: "CheckIns",
+                column: "AthleteId",
+                principalTable: "Athletes",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheckIns_Coaches_CoachId",
+                table: "CheckIns",
+                column: "CoachId",
+                principalTable: "Coaches",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheckIns_Athletes_AthleteId",
+                table: "CheckIns");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_CheckIns_Coaches_CoachId",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "ActivityAdherence",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "AffectedMuscleOrSession",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "AverageWeight",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "DiastolicBloodPressure",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "DietAdherence",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "EnergyLevel",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "FastingGlucoseLevels",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "HungerLevel",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "MenstrualCyclePhase",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "MuscleRecovery",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "NextWeekChallenges",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "OffPlanMeals",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "OtherInformation",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "PlansToAddressChalllenges",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "ProgressPictureURLs",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "SleepQuality",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "StressLevel",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "SystolicBloodPressure",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "WeeklyAchievements",
+                table: "CheckIns");
+
+            migrationBuilder.DropColumn(
+                name: "WeeklyChallenges",
+                table: "CheckIns");
+
+            migrationBuilder.RenameColumn(
+                name: "CoachId",
+                table: "CheckIns",
+                newName: "CoachID");
+
+            migrationBuilder.RenameColumn(
+                name: "AthleteId",
+                table: "CheckIns",
+                newName: "AthleteID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CheckIns_CoachId",
+                table: "CheckIns",
+                newName: "IX_CheckIns_CoachID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_CheckIns_AthleteId",
+                table: "CheckIns",
+                newName: "IX_CheckIns_AthleteID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheckIns_Athletes_AthleteID",
+                table: "CheckIns",
+                column: "AthleteID",
+                principalTable: "Athletes",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CheckIns_Coaches_CoachID",
+                table: "CheckIns",
+                column: "CoachID",
+                principalTable: "Coaches",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- BukenTrainerPortal/Models/CheckIn.cs
+++ BukenTrainerPortal/Models/CheckIn.cs
@@ -1,12 +1,124 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BukenTrainerPortal.Models
 {
+    public enum MenstrualCyclePhase
+    {
+        Menstrual,
+        Folicular,
+        Obulation,
+        Luteal,
+    }
+
+    public enum HungerLevel
+    {
+        [Description(
+            "Senti hambre en varias ocasiones durante el dia, las comidas no me llenaban y pasaba bastante tiempo esperando con ansias la siguiente comida"
+        )]
+        VeryHungryThroughoutDay,
+
+        [Description(
+            "Senti mucha hambre entre mis comidas, y estas me llenaban solo momentaneamente"
+        )]
+        VeryHungryBetweenMeals,
+
+        [Description("Senti poca hambre durante el dia, por lo general justo antes de mis comidas")]
+        LittleHungerDuringDay,
+
+        [Description(
+            "Me senti satisfecho durante todo el dia esta semana. Ni lleno ni con hambre."
+        )]
+        SatisfiedThroughoutDay,
+
+        [Description("Me senti levemente lleno despues de mis comidas, pero de manera placentera")]
+        SlightlyFullAfterMeals,
+
+        [Description("Me senti incomodamente lleno despues de algunas comidas del dia")]
+        UncomfortablyFullAfterMeals,
+    }
+
     public class CheckIn
     {
+        [Key]
         public int ID { get; set; }
+
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
 
+        [ForeignKey("AthleteId")]
         public required Athlete Athlete { get; set; }
+        public int AthleteId { get; set; }
+
+        [ForeignKey("CoachId")]
         public required Coach Coach { get; set; }
+        public int CoachId { get; set; }
+
+        [DisplayName("Weekly Achievements")]
+        public string? WeeklyAchievements { get; set; }
+
+        [DisplayName("Weekly Challenges")]
+        public string? WeeklyChallenges { get; set; }
+
+        [DisplayName("Next week's challenges")]
+        public string? NextWeekChallenges { get; set; }
+
+        [DisplayName("Plans to address challenges")]
+        public string? PlansToAddressChalllenges { get; set; }
+
+        [DisplayName("Other information")]
+        public string? OtherInformation { get; set; }
+
+        [DisplayName("Energy Levels")]
+        [Range(1, 10, ErrorMessage = "Energy level must be between 1 and 10")]
+        public int? EnergyLevel { get; set; }
+
+        [DisplayName("Stress Levels")]
+        [Range(1, 5, ErrorMessage = "Stress level must be between 1 and 5")]
+        public int? StressLevel { get; set; }
+
+        [DisplayName("Sleep Quality")]
+        [Range(1, 5, ErrorMessage = "Sleep quality must be between 1 and 5")]
+        public int? SleepQuality { get; set; }
+
+        [DisplayName("Diet Adherence")]
+        [Range(1, 5, ErrorMessage = "Diet adherence must be between 1 and 5")]
+        public int? DietAdherence { get; set; }
+
+        [DisplayName("Off Plan Meals")]
+        public string? OffPlanMeals { get; set; }
+
+        [DisplayName("Adherencia al plan de actividad")]
+        [Range(1, 5, ErrorMessage = "Activity adherence must be between 1 and 5")]
+        public int? ActivityAdherence { get; set; }
+
+        [DisplayName("Hunger Levels")]
+        public HungerLevel? HungerLevel { get; set; }
+
+        [DisplayName("Muscle Recovery")]
+        [Range(1, 10, ErrorMessage = "Diet adherence must be between 1 and 5")]
+        public int? MuscleRecovery { get; set; }
+
+        [DisplayName("Affected Muscles or Sessions?")]
+        public string? AffectedMuscleOrSession { get; set; }
+
+        [DisplayName("Average Bodyweight")]
+        public decimal? AverageWeight { get; set; }
+
+        [DisplayName("Menstrual Phase Cycle")]
+        public MenstrualCyclePhase? MenstrualCyclePhase { get; set; }
+
+        [DisplayName("Progres Picture URLs (Comma Separated)")]
+        public string? ProgressPictureURLs { get; set; }
+
+        [DisplayName("Systolic Blood Pressure (Optional)")]
+        public short? SystolicBloodPressure { get; set; }
+
+        [DisplayName("Diastolic Blood Pressure (Optional)")]
+        public short? DiastolicBloodPressure { get; set; }
+
+        [DisplayName("Fasting Glucose Levels (Optional)")]
+        public decimal? FastingGlucoseLevels { get; set; }
     }
 }
